f82b5b5fb03da912de9ab96aef4a8a22a44d4907
David Neal <davidneal@Davids-iMac.local>
2017-09-25 17:24:31 -0400

    Contact page added

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
 const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
   res.render("landing");
@@ -16,6 +19,14 @@ app.get("/about", (req, res) => {
 
 app.get("/gallery", (req, res) => {
   res.render("gallery");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+app.post("/contact", (req, res) => {
+  res.send("Email sending goes here.");
 });
 
 app.get("*", (req, res) => {
