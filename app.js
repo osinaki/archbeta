cc52f04b447c8d350c357d4c298e948bcb377369
David Neal <davidneal@Davids-iMac.local>
2017-10-12 10:02:15 -0400

    Added email functionality to contact page

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var nodemailer = require("nodemailer");
 
 const port = process.env.PORT || 3000;
 
@@ -26,7 +27,41 @@ app.get("/contact", (req, res) => {
 });
 
 app.post("/contact", (req, res) => {
-  res.send("Email sending goes here.");
+  // res.send("Email sending goes here.");
+  if (req.body.company) {
+    res.render("contact");
+    return;
+  }
+
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    secure: false,
+    port: 25,
+    auth: {
+      user: 'archbetaapts@gmail.com',
+      pass: 'Archbeta!1'
+    },
+    tls: {
+      rejectUnauthorized: false
+    }
+  });
+
+  let HelperOptions = {
+    from: req.body.email,
+    to: 'david.a.neal@hotmail.com',
+    subject: 'Contact Form Request from ' + req.body.email,
+    text: req.body.comment
+  };
+
+  transporter.sendMail(HelperOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("The message was sent!");
+    console.log(req.body);
+    console.log(info);
+    res.render("sent");
+  });
 });
 
 app.get("*", (req, res) => {
